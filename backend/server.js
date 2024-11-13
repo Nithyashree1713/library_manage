11b81cb7218817522ea3de6ee4920658d1bacbea
Nithyashree1713 <154865742+Nithyashree1713@users.noreply.github.com>
2024-11-13 10:05:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,9 @@ const app = express();
 app.use(express.json());
 app.use("/api", router);
 // app.use('/favorites', favoriteRoutes);
+const PORT = process.env.PORT || 5000;
 
-
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
     console.log(`Server started at http://localhost:5000`);
 });
